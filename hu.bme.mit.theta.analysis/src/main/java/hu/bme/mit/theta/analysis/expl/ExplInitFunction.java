farkasrebus/theta

commit 23bba368347c32e6328288192c505d96aba23172

Improved ExprStates, allowing to simplify transfer functions

hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/expl/ExplInitFunction.java
@@ -5,28 +5,33 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import java.util.Collection;
 
 import hu.bme.mit.theta.analysis.InitFunction;
+import hu.bme.mit.theta.analysis.expr.ExprStates;
 import hu.bme.mit.theta.core.Expr;
 import hu.bme.mit.theta.core.type.booltype.BoolType;
 import hu.bme.mit.theta.solver.Solver;
 
-public final class ExplInitFunction implements InitFunction<ExplState, ExplPrec> {
+public final class ExplInitFunction
+		implements InitFunction<ExplState, ExplPrec> {
 
 	private final Solver solver;
 	private final Expr<BoolType> initExpr;
 
-	private ExplInitFunction(final Solver solver, final Expr<BoolType> initExpr) {
+	private ExplInitFunction(final Solver solver,
+			final Expr<BoolType> initExpr) {
 		this.solver = checkNotNull(solver);
 		this.initExpr = checkNotNull(initExpr);
 	}
 
-	public static ExplInitFunction create(final Solver solver, final Expr<BoolType> initExpr) {
+	public static ExplInitFunction create(final Solver solver,
+			final Expr<BoolType> initExpr) {
 		return new ExplInitFunction(solver, initExpr);
 	}
 
 	@Override
 	public Collection<? extends ExplState> getInitStates(final ExplPrec prec) {
 		checkNotNull(prec);
-		return prec.createStatesForExpr(solver, initExpr);
+		return ExprStates.createStatesForExpr(solver, initExpr, 0,
+				prec::createState, 0);
 	}
 
 }

hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/expl/ExplTransferFunction.java
@@ -1,18 +1,17 @@
 package hu.bme.mit.theta.analysis.expl;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static hu.bme.mit.theta.core.type.booltype.BoolExprs.Not;
 
 import java.util.Collection;
-import java.util.HashSet;
 
 import hu.bme.mit.theta.analysis.TransferFunction;
 import hu.bme.mit.theta.analysis.expr.ExprAction;
-import hu.bme.mit.theta.core.model.impl.Valuation;
-import hu.bme.mit.theta.core.utils.PathUtils;
+import hu.bme.mit.theta.analysis.expr.ExprStates;
+import hu.bme.mit.theta.core.type.booltype.BoolExprs;
 import hu.bme.mit.theta.solver.Solver;
 
-public final class ExplTransferFunction implements TransferFunction<ExplState, ExprAction, ExplPrec> {
+public final class ExplTransferFunction
+		implements TransferFunction<ExplState, ExprAction, ExplPrec> {
 
 	private final Solver solver;
 
@@ -25,32 +24,14 @@ public final class ExplTransferFunction implements TransferFunction<ExplState, E
 	}
 
 	@Override
-	public Collection<? extends ExplState> getSuccStates(final ExplState state, final ExprAction action,
-			final ExplPrec prec) {
+	public Collection<? extends ExplState> getSuccStates(final ExplState state,
+			final ExprAction action, final ExplPrec prec) {
 		checkNotNull(state);
 		checkNotNull(action);
 		checkNotNull(prec);
-
-		final Collection<ExplState> succStates = new HashSet<>();
-
-		solver.push();
-		solver.add(PathUtils.unfold(state.toExpr(), 0));
-		solver.add(PathUtils.unfold(action.toExpr(), 0));
-
-		boolean moreSuccStates;
-		do {
-			moreSuccStates = solver.check().isSat();
-			if (moreSuccStates) {
-				final Valuation nextSuccStateVal = PathUtils.extractValuation(solver.getModel(), action.nextIndexing(),
-						prec.getVars());
-				final ExplState nextSuccState = prec.createState(nextSuccStateVal);
-				succStates.add(nextSuccState);
-				solver.add(PathUtils.unfold(Not(nextSuccState.toExpr()), action.nextIndexing()));
-			}
-		} while (moreSuccStates);
-		solver.pop();
-
-		return succStates;
+		return ExprStates.createStatesForExpr(solver,
+				BoolExprs.And(state.toExpr(), action.toExpr()), 0,
+				prec::createState, 1);
 	}
 
 }

hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/expr/ExprStates.java
@@ -14,24 +14,40 @@ import hu.bme.mit.theta.core.type.booltype.BoolType;
 import hu.bme.mit.theta.core.utils.PathUtils;
 import hu.bme.mit.theta.solver.Solver;
 
+/**
+ * Utility for generating ExprStates.
+ */
 public final class ExprStates {
 
 	private ExprStates() {
 	}
 
-	public static <S extends ExprState> Collection<S> createStatesForExpr(final Solver solver,
-			final Expr<BoolType> expr, final Function<? super Valuation, ? extends S> valuationToState) {
+	/**
+	 * Generate all states that satisfy a given expression.
+	 * 
+	 * @param solver Solver
+	 * @param expr Expression to be satisfied
+	 * @param exprIndex Index for unfolding the expression
+	 * @param valuationToState Mapping from a valuation to a state
+	 * @param stateIndex Index for extracting the state
+	 * @return States satisfying the expression
+	 */
+	public static <S extends ExprState> Collection<S> createStatesForExpr(
+			final Solver solver, final Expr<BoolType> expr, final int exprIndex,
+			final Function<? super Valuation, ? extends S> valuationToState,
+			final int stateIndex) {
 
 		return using(solver, s -> {
-			s.add(PathUtils.unfold(expr, 0));
+			s.add(PathUtils.unfold(expr, exprIndex));
 
 			final Collection<S> result = new ArrayList<>();
 			while (s.check().isSat()) {
 				final Model model = s.getModel();
-				final Valuation valuation = PathUtils.extractValuation(model, 0);
+				final Valuation valuation = PathUtils.extractValuation(model,
+						stateIndex);
 				final S state = valuationToState.apply(valuation);
 				result.add(state);
-				s.add(Not(PathUtils.unfold(state.toExpr(), 0)));
+				s.add(Not(PathUtils.unfold(state.toExpr(), stateIndex)));
 			}
 
 			return result;

hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/pred/PredInitFunction.java
@@ -5,28 +5,33 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import java.util.Collection;
 
 import hu.bme.mit.theta.analysis.InitFunction;
+import hu.bme.mit.theta.analysis.expr.ExprStates;
 import hu.bme.mit.theta.core.Expr;
 import hu.bme.mit.theta.core.type.booltype.BoolType;
 import hu.bme.mit.theta.solver.Solver;
 
-public final class PredInitFunction implements InitFunction<PredState, PredPrec> {
+public final class PredInitFunction
+		implements InitFunction<PredState, PredPrec> {
 
 	private final Solver solver;
 	private final Expr<BoolType> initExpr;
 
-	private PredInitFunction(final Solver solver, final Expr<BoolType> initExpr) {
+	private PredInitFunction(final Solver solver,
+			final Expr<BoolType> initExpr) {
 		this.solver = checkNotNull(solver);
 		this.initExpr = checkNotNull(initExpr);
 	}
 
-	public static PredInitFunction create(final Solver solver, final Expr<BoolType> expr) {
+	public static PredInitFunction create(final Solver solver,
+			final Expr<BoolType> expr) {
 		return new PredInitFunction(solver, expr);
 	}
 
 	@Override
 	public Collection<? extends PredState> getInitStates(final PredPrec prec) {
 		checkNotNull(prec);
-		return prec.createStatesForExpr(solver, initExpr);
+		return ExprStates.createStatesForExpr(solver, initExpr, 0,
+				prec::createState, 0);
 	}
 
 }

hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/pred/PredTransferFunction.java
@@ -1,19 +1,17 @@
 package hu.bme.mit.theta.analysis.pred;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static hu.bme.mit.theta.core.type.booltype.BoolExprs.Not;
 
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 
 import hu.bme.mit.theta.analysis.TransferFunction;
 import hu.bme.mit.theta.analysis.expr.ExprAction;
-import hu.bme.mit.theta.core.model.impl.Valuation;
-import hu.bme.mit.theta.core.utils.PathUtils;
+import hu.bme.mit.theta.analysis.expr.ExprStates;
+import hu.bme.mit.theta.core.type.booltype.BoolExprs;
 import hu.bme.mit.theta.solver.Solver;
 
-public final class PredTransferFunction implements TransferFunction<PredState, ExprAction, PredPrec> {
+public final class PredTransferFunction
+		implements TransferFunction<PredState, ExprAction, PredPrec> {
 
 	private final Solver solver;
 
@@ -26,31 +24,15 @@ public final class PredTransferFunction implements TransferFunction<PredState, E
 	}
 
 	@Override
-	public Collection<? extends PredState> getSuccStates(final PredState state, final ExprAction action,
-			final PredPrec prec) {
+	public Collection<? extends PredState> getSuccStates(final PredState state,
+			final ExprAction action, final PredPrec prec) {
 		checkNotNull(state);
 		checkNotNull(action);
 		checkNotNull(prec);
 
-		final Set<PredState> succStates = new HashSet<>();
-		solver.push();
-		solver.add(PathUtils.unfold(state.toExpr(), 0));
-		solver.add(PathUtils.unfold(action.toExpr(), 0));
-
-		boolean moreSuccStates;
-		do {
-			moreSuccStates = solver.check().isSat();
-			if (moreSuccStates) {
-				final Valuation nextSuccStateVal = PathUtils.extractValuation(solver.getModel(), action.nextIndexing());
-
-				final PredState nextSuccState = prec.createState(nextSuccStateVal);
-				succStates.add(nextSuccState);
-				solver.add(PathUtils.unfold(Not(nextSuccState.toExpr()), action.nextIndexing()));
-			}
-		} while (moreSuccStates);
-		solver.pop();
-
-		return succStates;
+		return ExprStates.createStatesForExpr(solver,
+				BoolExprs.And(state.toExpr(), action.toExpr()), 0,
+				prec::createState, 1);
 	}
 
 }

hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/expl/ExplPrec.java
@@ -10,14 +10,10 @@ import java.util.Set;
 import com.google.common.collect.ImmutableSet;
 
 import hu.bme.mit.theta.analysis.Prec;
-import hu.bme.mit.theta.analysis.expr.ExprStates;
 import hu.bme.mit.theta.common.ObjectUtils;
-import hu.bme.mit.theta.core.Expr;
 import hu.bme.mit.theta.core.LitExpr;
 import hu.bme.mit.theta.core.decl.VarDecl;
 import hu.bme.mit.theta.core.model.impl.Valuation;
-import hu.bme.mit.theta.core.type.booltype.BoolType;
-import hu.bme.mit.theta.solver.Solver;
 
 /**
  * Represents an immutable, simple explicit precision that is a set of
@@ -51,8 +47,8 @@ public final class ExplPrec implements Prec {
 
 	public ExplPrec join(final ExplPrec other) {
 		checkNotNull(other);
-		final Collection<VarDecl<?>> newVars = ImmutableSet.<VarDecl<?>>builder().addAll(vars).addAll(other.vars)
-				.build();
+		final Collection<VarDecl<?>> newVars = ImmutableSet
+				.<VarDecl<?>>builder().addAll(vars).addAll(other.vars).build();
 		// If no new variable was added, return same instance (immutable)
 		if (newVars.size() == this.vars.size()) {
 			return this;
@@ -75,15 +71,10 @@ public final class ExplPrec implements Prec {
 		return ExplState.create(builder.build());
 	}
 
-	public Collection<ExplState> createStatesForExpr(final Solver solver, final Expr<BoolType> expr) {
-		checkNotNull(solver);
-		checkNotNull(expr);
-		return ExprStates.createStatesForExpr(solver, expr, this::createState);
-	}
-
 	@Override
 	public String toString() {
-		return ObjectUtils.toStringBuilder(getClass().getSimpleName()).addAll(vars, VarDecl::getName).toString();
+		return ObjectUtils.toStringBuilder(getClass().getSimpleName())
+				.addAll(vars, VarDecl::getName).toString();
 	}
 
 	@Override

hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/pred/PredPrec.java
@@ -1,19 +1,10 @@
 package hu.bme.mit.theta.analysis.pred;
 
-import java.util.Collection;
-
 import hu.bme.mit.theta.analysis.Prec;
-import hu.bme.mit.theta.analysis.expr.ExprStates;
-import hu.bme.mit.theta.core.Expr;
 import hu.bme.mit.theta.core.model.impl.Valuation;
-import hu.bme.mit.theta.core.type.booltype.BoolType;
-import hu.bme.mit.theta.solver.Solver;
 
 public interface PredPrec extends Prec {
 
 	PredState createState(final Valuation valuation);
 
-	default Collection<? extends PredState> createStatesForExpr(final Solver solver, final Expr<BoolType> expr) {
-		return ExprStates.createStatesForExpr(solver, expr, this::createState);
-	}
 }

commit a53e65e942e29306322a58c0f2528e09070c49d0

Fixed a critical bug in abstract successor computation
It was introduced with commit 23bba36 because the successor states were
always extracted with index 1. But for CFAs if a variable is not
modified, its index will not increase.

hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/expl/ExplInitFunction.java
@@ -8,6 +8,7 @@ import hu.bme.mit.theta.analysis.InitFunction;
 import hu.bme.mit.theta.analysis.expr.ExprStates;
 import hu.bme.mit.theta.core.Expr;
 import hu.bme.mit.theta.core.type.booltype.BoolType;
+import hu.bme.mit.theta.core.utils.VarIndexing;
 import hu.bme.mit.theta.solver.Solver;
 
 public final class ExplInitFunction implements InitFunction<ExplState, ExplPrec> {
@@ -27,7 +28,7 @@ public final class ExplInitFunction implements InitFunction<ExplState, ExplPrec>
 	@Override
 	public Collection<? extends ExplState> getInitStates(final ExplPrec prec) {
 		checkNotNull(prec);
-		return ExprStates.createStatesForExpr(solver, initExpr, 0, prec::createState, 0);
+		return ExprStates.createStatesForExpr(solver, initExpr, 0, prec::createState, VarIndexing.all(0));
 	}
 
 }

hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/expl/ExplTransferFunction.java
@@ -29,7 +29,7 @@ public final class ExplTransferFunction implements TransferFunction<ExplState, E
 		checkNotNull(action);
 		checkNotNull(prec);
 		return ExprStates.createStatesForExpr(solver, BoolExprs.And(state.toExpr(), action.toExpr()), 0,
-				prec::createState, 1);
+				prec::createState, action.nextIndexing());
 	}
 
 }

hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/expr/ExprStates.java
@@ -12,6 +12,7 @@ import hu.bme.mit.theta.core.model.Model;
 import hu.bme.mit.theta.core.model.Valuation;
 import hu.bme.mit.theta.core.type.booltype.BoolType;
 import hu.bme.mit.theta.core.utils.PathUtils;
+import hu.bme.mit.theta.core.utils.VarIndexing;
 import hu.bme.mit.theta.solver.Solver;
 
 /**
@@ -24,17 +25,17 @@ public final class ExprStates {
 
 	/**
 	 * Generate all states that satisfy a given expression.
-	 * 
+	 *
 	 * @param solver Solver
 	 * @param expr Expression to be satisfied
 	 * @param exprIndex Index for unfolding the expression
 	 * @param valuationToState Mapping from a valuation to a state
-	 * @param stateIndex Index for extracting the state
+	 * @param stateIndexing Index for extracting the state
 	 * @return States satisfying the expression
 	 */
 	public static <S extends ExprState> Collection<S> createStatesForExpr(final Solver solver,
 			final Expr<BoolType> expr, final int exprIndex,
-			final Function<? super Valuation, ? extends S> valuationToState, final int stateIndex) {
+			final Function<? super Valuation, ? extends S> valuationToState, final VarIndexing stateIndexing) {
 
 		return using(solver, s -> {
 			s.add(PathUtils.unfold(expr, exprIndex));
@@ -42,10 +43,10 @@ public final class ExprStates {
 			final Collection<S> result = new ArrayList<>();
 			while (s.check().isSat()) {
 				final Model model = s.getModel();
-				final Valuation valuation = PathUtils.extractValuation(model, stateIndex);
+				final Valuation valuation = PathUtils.extractValuation(model, stateIndexing);
 				final S state = valuationToState.apply(valuation);
 				result.add(state);
-				s.add(Not(PathUtils.unfold(state.toExpr(), stateIndex)));
+				s.add(Not(PathUtils.unfold(state.toExpr(), stateIndexing)));
 			}
 
 			return result;

hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/pred/PredInitFunction.java
@@ -8,6 +8,7 @@ import hu.bme.mit.theta.analysis.InitFunction;
 import hu.bme.mit.theta.analysis.expr.ExprStates;
 import hu.bme.mit.theta.core.Expr;
 import hu.bme.mit.theta.core.type.booltype.BoolType;
+import hu.bme.mit.theta.core.utils.VarIndexing;
 import hu.bme.mit.theta.solver.Solver;
 
 public final class PredInitFunction implements InitFunction<PredState, PredPrec> {
@@ -27,7 +28,7 @@ public final class PredInitFunction implements InitFunction<PredState, PredPrec>
 	@Override
 	public Collection<? extends PredState> getInitStates(final PredPrec prec) {
 		checkNotNull(prec);
-		return ExprStates.createStatesForExpr(solver, initExpr, 0, prec::createState, 0);
+		return ExprStates.createStatesForExpr(solver, initExpr, 0, prec::createState, VarIndexing.all(0));
 	}
 
 }

hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/pred/PredTransferFunction.java
@@ -30,7 +30,7 @@ public final class PredTransferFunction implements TransferFunction<PredState, E
 		checkNotNull(prec);
 
 		return ExprStates.createStatesForExpr(solver, BoolExprs.And(state.toExpr(), action.toExpr()), 0,
-				prec::createState, 1);
+				prec::createState, action.nextIndexing());
 	}
 
 }